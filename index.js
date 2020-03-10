8ec1d6ac54ee0e5ffb1e03f032c441c5812cb74a
Jorge Basilio <J.l.Basilio55@gmail.com>
2020-03-10 09:04:57 -0400

    Done.

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const input = document.querySelector('input');
 
 function addingEventListener() {
   input.addEventListener('click', function(event) {
-    return "clicked"
+    alert("clicked") 
   });
 }
 
